be6b22a2250119d2b051d7ed25f4dae18d77700d
Midlou <63810404+Midlou@users.noreply.github.com>
2020-04-28 10:18:17 -0300

    27/04/2020
    Updated Download Area
    Added Download subarea: Versions
    Updated Scripts

--- miceblocks-website/script.js
+++ miceblocks-website/script.js
@@ -1,70 +1,34 @@
-function NavDownloadOn(name) {
-  //console.log("On");
+function ButtonOn(name) {
   var image = document.getElementById(name);
-  image.src = "imgs/download-button-selected.png";
+  image.src = "imgs/" + name + "-on.png";
 }
 
-function NavDownloadOff(name) {
-  //console.log("Off");
+function ButtonOff(name) {
   var image = document.getElementById(name);
-  image.src = "imgs/download-button-unselected.png";
+  image.src = "imgs/" + name + "-off.png";
 }
 
-function NavChangelogOn(name) {
-  //console.log("On");
-  var image = document.getElementById(name);
-  image.src = "imgs/changelog-button-selected.png";
-}
-
-function NavChangelogOff(name) {
-  //console.log("Off");
-  var image = document.getElementById(name);
-  image.src = "imgs/changelog-button-unselected.png";
-}
-
-function NavInfoOn(name) {
-  //console.log("On");
-  var image = document.getElementById(name);
-  image.src = "imgs/info-button-selected.png";
-}
-
-function NavInfoOff(name) {
-  //console.log("Off");
-  var image = document.getElementById(name);
-  image.src = "imgs/info-button-unselected.png";
+function DownloadVersionToggle() {
+  var x = document.getElementById("download-versions-id");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 
-
-function DownloadButtonOn(number) {
-    //console.log("On");
-    var image = document.getElementById("download-button-image" + number);
-    image.src = "imgs/download-icon-green.png";
-}
-
-function DownloadButtonOff(number) {
-    //console.log("Off");
-    var image = document.getElementById("download-button-image" + number);
-    image.src = "imgs/download-icon-gold.png";
-}
-
-
-
-//Get the button
-var mybutton = document.getElementById("top-arrow-id");
-
-// When the user scrolls down 20px from the top of the document, show the button
+var topbutton = document.getElementById("top-arrow-id");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    topbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    topbutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
