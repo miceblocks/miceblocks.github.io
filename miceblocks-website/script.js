9f28a2beb85c7ebee56fa87e8ed045d0addcca1e
Midlou <63810404+Midlou@users.noreply.github.com>
2020-05-02 18:35:22 -0300

    01/05/2020
    Added image carousel
    Minor optimizations

--- miceblocks-website/script.js
+++ miceblocks-website/script.js
@@ -35,3 +35,44 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+var slideIndex = 1;
+var headerId = document.getElementById("header-id"); 
+var headerNavId = document.getElementById("header-nav-id");
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carousel-images"); 
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  if(slideIndex == 1) {
+    headerId.style.backgroundColor = "#8aacff";
+    headerNavId.style.backgroundColor = "#8aacff";
+  }
+  else if(slideIndex == 2) {
+    headerId.style.backgroundColor = "#4f659b";
+    headerNavId.style.backgroundColor = "#4f659b";
+  }
+  else if(slideIndex == 3) {
+    headerId.style.backgroundColor = "#546395";
+    headerNavId.style.backgroundColor = "#546395";
+  }
+
+  
+  slides[slideIndex-1].style.display = "block";
+}
