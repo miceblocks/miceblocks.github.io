8e113f5bb6a18c318297c6d6d0b1dd4c633286b4
Midlou <63810404+Midlou@users.noreply.github.com>
2020-05-04 10:27:06 -0300

    03/05/2020
    Updated Carousel Area

--- miceblocks-website/script.js
+++ miceblocks-website/script.js
@@ -21,58 +21,79 @@ function SpoilerVersionToggle(name) {
 
 
 var topbutton = document.getElementById("top-arrow-id");
+var scrollTest = true;
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topbutton.style.display = "block";
+  if (scrollTest == true) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topbutton.style.display = "block";
+    } else {
+      topbutton.style.display = "none";
+    }
   } else {
-    topbutton.style.display = "none";
+    setTimeout(function(){
+      scrollTest = true;
+    }, 1000);
   }
 }
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+  topbutton.style.display = "none";
+  scrollTest = false;
 }
 
-
-var slideIndex = 1;
+var slideIndex = 1
+var arrowsId = document.getElementsByClassName("carousel-arrows");
+var imageId = document.getElementById("carousel-images-id");
 var headerId = document.getElementById("header-id"); 
 var headerNavId = document.getElementById("header-nav-id");
-showSlides(slideIndex);
+var slideTimerVar;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function SlideTimer() {
+  slideTimerVar = setTimeout(function(){
+    arrowsId[0].click();
+    SlideTimer();
+  }, 7000);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("carousel-images"); 
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
+SlideTimer();
 
+function showSlides(n) {
+  clearTimeout(slideTimerVar);
+  for (i = 0; i < arrowsId.length; i++) {
+    arrowsId[i].disabled = true;
+    arrowsId[i].style.opacity = 0.3;
+  }
+  slideIndex += n;
+  if (slideIndex > 3) {
+    slideIndex = 1;
+  }
+  else if (slideIndex < 1) {
+    slideIndex = 3;
+  }
   if(slideIndex == 1) {
     headerId.style.backgroundColor = "#8aacff";
     headerNavId.style.backgroundColor = "#8aacff";
+    imageId.style.backgroundImage = "url('imgs/background-main.jpg')";
   }
   else if(slideIndex == 2) {
     headerId.style.backgroundColor = "#4f659b";
     headerNavId.style.backgroundColor = "#4f659b";
+    imageId.style.backgroundImage = "url('imgs/background1.jpg')";
   }
   else if(slideIndex == 3) {
     headerId.style.backgroundColor = "#546395";
     headerNavId.style.backgroundColor = "#546395";
+    imageId.style.backgroundImage = "url('imgs/background2.jpg')";
   }
-
   
-  slides[slideIndex-1].style.display = "block";
+  setTimeout(function(){
+    for (i = 0; i < arrowsId.length; i++) {
+      arrowsId[i].disabled = false;
+      arrowsId[i].style.opacity = 1;
+    }
+  }, 1500);
 }
