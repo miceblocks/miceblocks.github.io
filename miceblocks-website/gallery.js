f750ccba2c7397fa6f0eba5fc9cf00907be55966
Midlou <63810404+Midlou@users.noreply.github.com>
2020-05-13 08:44:46 -0300

    12/05/2020
    Updated gallery area

--- miceblocks-website/gallery.js
+++ miceblocks-website/gallery.js
@@ -1,80 +1,48 @@
-function initComparisons() {
-    var x, i;
-    /* Find all elements with an "overlay" class: */
-    x = document.getElementsByClassName("img-comp-overlay");
-    for (i = 0; i < x.length; i++) {
-      /* Once for each "overlay" element:
-      pass the "overlay" element as a parameter when executing the compareImages function: */
-      compareImages(x[i]);
-    }
+/*================Carousel Area / Gallery ================*/
+var slideIndex = 1;
+var arrowsId = document.getElementsByClassName("gallery-carousel-arrows");
+var imageId = document.getElementById("gallery-carousel-id");
+var imageListId = document.getElementsByClassName("gallery-image-list");
+var slideTimerVar;
 
-    function compareImages(img) {
-      var slider, img, clicked = 0, w, h;
-      /* Get the width and height of the img element */
-      w = img.offsetWidth;
-      h = img.offsetHeight;
-      /* Set the width of the img element to 50%: */
-      img.style.width = (w / 2) + "px";
-      /* Create slider: */
-      slider = document.createElement("DIV");
-      slider.setAttribute("class", "img-comp-slider");
-      /* Insert slider */
-      img.parentElement.insertBefore(slider, img);
-      /* Position the slider in the middle: */
-      slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-      slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
-      /* Execute a function when the mouse button is pressed: */
-      slider.addEventListener("mousedown", slideReady);
-      /* And another function when the mouse button is released: */
-      window.addEventListener("mouseup", slideFinish);
-      /* Or touched (for touch screens: */
-      slider.addEventListener("touchstart", slideReady);
-       /* And released (for touch screens: */
-      window.addEventListener("touchstop", slideFinish);
-      function slideReady(e) {
-        /* Prevent any other actions that may occur when moving over the image: */
-        e.preventDefault();
-        /* The slider is now clicked and ready to move: */
-        clicked = 1;
-        /* Execute a function when the slider is moved: */
-        window.addEventListener("mousemove", slideMove);
-        window.addEventListener("touchmove", slideMove);
-      }
-      function slideFinish() {
-        /* The slider is no longer clicked: */
-        clicked = 0;
-      }
 
-      function slideMove(e) {
-        var pos;
-        /* If the slider is no longer clicked, exit this function: */
-        if (clicked == 0) return false;
-        /* Get the cursor's x position: */
-        pos = getCursorPos(e)
-        /* Prevent the slider from being positioned outside the image: */
-        if (pos < 0) pos = 0;
-        if (pos > w) pos = w;
-        /* Execute a function that will resize the overlay image according to the cursor: */
-        slide(pos);
-      }
+function GallerySlideTimer() {
+  slideTimerVar = setTimeout(function(){
+    arrowsId[0].click();
+    GallerySlideTimer();
+  }, 6000);
+}
 
-      function getCursorPos(e) {
-        var a, x = 0;
-        e = e || window.event;
-        /* Get the x positions of the image: */
-        a = img.getBoundingClientRect();
-        /* Calculate the cursor's x coordinate, relative to the image: */
-        x = e.pageX - a.left;
-        /* Consider any page scrolling: */
-        x = x - window.pageXOffset;
-        return x;
-      }
+GallerySlideTimer();
 
-      function slide(x) {
-        /* Resize the image: */
-        img.style.width = x + "px";
-        /* Position the slider: */
-        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
-      }
-    }
+function galleryShowSlides(n, slide) {
+  clearTimeout(slideTimerVar);
+  for (i = 0; i < arrowsId.length; i++) {
+    arrowsId[i].disabled = true;
+    arrowsId[i].style.opacity = 0.3;
   }
+  if (slide == true) {
+    slideIndex += n;
+    if (slideIndex > 15) {
+      slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+      slideIndex = 15;
+    }
+  } else {
+    slideIndex = n;
+  }
+  imageId.style.backgroundImage = "url('imgs/screenshots/screenshot-" + slideIndex + ".jpg')";
+
+  for (i = 0; i < 15; i++) {
+    imageListId[i].style.opacity = ".2";
+  }
+  imageListId[slideIndex-1].style.opacity = "1";
+  
+  setTimeout(function(){
+    for (i = 0; i < arrowsId.length; i++) {
+      arrowsId[i].disabled = false;
+      arrowsId[i].style.opacity = 1;
+    }
+  }, 1500);
+}
