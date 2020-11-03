8ba6c2eb091ebba1868fa4bfbabdecf4c4b27976
Midlou <63810404+Midlou@users.noreply.github.com>
2020-11-03 20:18:45 -0300

    Added vA.12.1 download link, i'm dumb

--- js/angular_controllers.js
+++ js/angular_controllers.js
@@ -13,5 +13,11 @@ app.controller('myCtrl', function($scope) {
             "minecraft": "[1.13] ~ [1.16.3]",
             "link": "http://adfoc.us/51312375319825"
         },
+
+        {
+            "version": "A.12.1",
+            "minecraft": "[1.13] ~ [1.16.4]",
+            "link": "http://adfoc.us/51312376464073"
+        },
     ]
 });
